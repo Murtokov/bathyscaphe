5e3c7cccb6109248ec404bb11f3d9849e5702b04
Somkin Artem <somkinartem@yandex.ru>
2025-10-09 21:52:52 +0300

    added ladder

---
+++ Assets/scripts/level1/LadderUp.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class LadderUp : MonoBehaviour
+{
+    public bool inTrigger = false;
+    public GameObject contextHint;
+    public void Start()
+    {
+        contextHint = transform.GetChild(0).gameObject;
+        contextHint.SetActive(false);
+    }
+    public void Update()
+    {
+        if (inTrigger)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Transform player = GameObject.FindGameObjectWithTag("Player").transform;
+                contextHint.SetActive(false);
+                player.position = new Vector3(-7, 12, 0);
+            }
+        }
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            inTrigger = true;
+            contextHint.SetActive(true);
+        }
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            inTrigger = false;
+            contextHint.SetActive(false);
+        }
+    }
+}
